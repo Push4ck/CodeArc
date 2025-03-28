ab86cdb0b59a6c4edb1bfd67671cabc1da21e86c
Pushkar Sharma <ps100016@google.com>
2025-03-28 08:07:17 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import NodePolyfills from "vite-plugin-node-polyfills";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), NodePolyfills()],
   resolve: {
     alias: {
       url: "url-polyfill",
       os: "os-browserify/browser",
-      process: "process/browser",
     },
-  },
-  define: {
-    "process.env": {},
   },
 });
